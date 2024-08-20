8c1544cdc536ef7fa8f48f62a819745af466d691
Advaith H L <niceadvaith@gmail.com>
2024-08-20 14:35:50 +0200

    Implement checklist

--- checkmate/src/components/Checklist.tsx
+++ checkmate/src/components/Checklist.tsx
@@ -1,3 +1,39 @@
+import { CheckIcon, TrashIcon } from "@radix-ui/react-icons";
+import { Button, Card, Container, Flex, Text } from "@radix-ui/themes";
+
 export function Checklist() {
-  return <></>
+  const data = [
+    {
+      id: "1",
+      text: "Hello world",
+    },
+    {
+      id: "3",
+      text: "Buy milk",
+    },
+  ];
+
+  return (
+    <Container>
+      {data.map((item) => (
+        <Card key={item.id} mx={"2"} my={"3"}>
+          <Flex justify={"between"} align={"center"}>
+            <Text ml={"3"} size={"3"}>
+              {item.text}
+            </Text>
+            <Flex gap={"2"} justify={"end"}>
+              <Button variant={"soft"} color={"grass"}>
+                <Text>Mark as done</Text>
+                <CheckIcon width="18" height="18" />
+              </Button>
+              <Button variant={"soft"} color={"red"}>
+                Delete
+                <TrashIcon width="18" height="18" />
+              </Button>
+            </Flex>
+          </Flex>
+        </Card>
+      ))}
+    </Container>
+  );
 }
