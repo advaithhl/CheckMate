7d43d4d33ac2fc144f4cb72b5495b58b1a1581a8
Advaith H L <niceadvaith@gmail.com>
2024-09-14 18:32:31 +0530

    Build Lambda deployment with py dependencies

---
+++ checkmate/deployUtils/BuildLambdaDeployment/docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Navigate to lambda directory
+cd lambda
+
+# Install pip and zip
+apt update
+apt install python3-pip -y
+apt install zip -y
+
+# Package dependencies as per https://docs.aws.amazon.com/lambda/latest/dg/python-package.html#python-package-create-dependencies
+mkdir package
+pip3 install -r requirements.txt --target ./package
+cd package
+zip -r ../my_deployment_package.zip .
+cd ..
+zip my_deployment_package.zip lambda_function.py
+
+# Remove the package with all dependencies
+rm -rf package
