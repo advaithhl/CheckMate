8fe49a0d2b93a2ce9b11c65e4b71998ff6d6de9d
Advaith H L <niceadvaith@gmail.com>
2024-09-17 23:20:18 +0530

    Extend API Gateway with action routes

---
+++ checkmate/infra/iam_api_gateway_actions.tf
@@ -0,0 +1,23 @@
+resource "aws_lambda_permission" "actions_add_item_permission" {
+  statement_id  = "AllowApiGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.actions_add_item.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.checkmate_api.execution_arn}/*/*"
+}
+
+resource "aws_lambda_permission" "actions_get_items_permission" {
+  statement_id  = "AllowApiGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.actions_get_items.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.checkmate_api.execution_arn}/*/*"
+}
+
+resource "aws_lambda_permission" "actions_delete_item_permission" {
+  statement_id  = "AllowApiGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.actions_delete_item.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.checkmate_api.execution_arn}/*/*"
+}
