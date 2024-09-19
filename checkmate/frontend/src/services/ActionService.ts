78b23dea592a269a6bcd6c510dedf8cf69fbe924
Advaith H L <niceadvaith@gmail.com>
2024-09-19 18:34:56 +0530

    Send JWT with each action request

--- checkmate/frontend/src/services/ActionService.ts
+++ checkmate/frontend/src/services/ActionService.ts
@@ -1,20 +1,34 @@
 import axios from "axios";
 import { ItemList, NewItem, ToDeleteItem } from "../models";
 import { APP_API_URL } from "../constants";
+import { getUserInfoFromLocalStorage } from "../utils/userInfo";
 
 export async function getItems(): Promise<ItemList> {
-  const response = await axios.get(APP_API_URL + "/actions/getItems");
+  const userInfo = getUserInfoFromLocalStorage();
+  const token = userInfo ? userInfo.token : "";
+  const response = await axios.get(APP_API_URL + "/actions/getItems", {
+    headers: { Authorization: `Bearer ${token}` },
+  });
   return response.data;
 }
 
 export async function addItem(item: NewItem) {
-  const response = await axios.post(APP_API_URL + "/actions/addItem", item);
+  const userInfo = getUserInfoFromLocalStorage();
+  const token = userInfo ? userInfo.token : "";
+  const response = await axios.post(APP_API_URL + "/actions/addItem", item, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
   return response.data;
 }
 
 export async function deleteItem(item: ToDeleteItem) {
+  const userInfo = getUserInfoFromLocalStorage();
+  const token = userInfo ? userInfo.token : "";
   const response = await axios.delete(
-    APP_API_URL + `/actions/deleteItem/${item.id}`
+    APP_API_URL + `/actions/deleteItem/${item.id}`,
+    {
+      headers: { Authorization: `Bearer ${token}` },
+    }
   );
   return response.data;
 }
