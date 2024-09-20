376c4b07561963f85c96c529e663117d658849f1
Advaith H L <niceadvaith@gmail.com>
2024-09-20 19:44:14 +0530

    Output API Gateway URL post tf application

--- checkmate/infra/outputs.tf
+++ checkmate/infra/outputs.tf
@@ -2,3 +2,8 @@ output "checkmate_url" {
   value       = "https://prod.${aws_amplify_app.checkmate_frontend.default_domain}"
   description = "The application URL to be used by end-users."
 }
+
+output "checkmate_backend_url" {
+  value       = aws_apigatewayv2_api.checkmate_api.api_endpoint
+  description = "The API Gateway URL to be provided to frontend during build."
+}
