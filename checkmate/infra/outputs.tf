e5949757a53697c141feb0ca9753f97a3814b2f1
Advaith H L <niceadvaith@gmail.com>
2024-09-20 22:53:52 +0530

    Output Amplify app ID post tf application

--- checkmate/infra/outputs.tf
+++ checkmate/infra/outputs.tf
@@ -7,3 +7,8 @@ output "checkmate_backend_url" {
   value       = aws_apigatewayv2_api.checkmate_api.api_endpoint
   description = "The API Gateway URL to be provided to frontend during build."
 }
+
+output "amplify_app_id" {
+  value       = aws_amplify_app.checkmate_frontend.id
+  description = "The Amplify app ID used for deploying frontend."
+}
